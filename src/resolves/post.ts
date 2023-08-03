6eb0f401c3dcfc8aa58e54bd1e5f2f8dd87fbe75
Shoaib Dar <SDAR30@gmail.com>
2023-08-03 16:35:31 -0400

    createPost mutation added

--- src/resolves/post.ts
+++ src/resolves/post.ts
@@ -1,4 +1,4 @@
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from "type-graphql";
 import { Post } from '../entities/Post';
 import { MyContext } from "src/types";
 
@@ -9,12 +9,22 @@ export class PostResolver {
         return em.find(Post, {})
     }
 
-    @Query(() => Post, {nullable: true})
+    @Query(() => Post, { nullable: true })
     post(
         @Arg('id', () => Int) id: number,
         @Ctx() { em }: MyContext
     ): Promise<Post | null> {
         return em.findOne(Post, { id });
+    }
+
+    @Mutation(() => Post)
+    async createPost(
+        @Arg('title') title: string,
+        @Ctx() { em }: MyContext
+    ): Promise<Post> {
+        const post = em.create(Post, { title } as Post);
+        await em.persistAndFlush(post)
+        return post
     }
 
 }
